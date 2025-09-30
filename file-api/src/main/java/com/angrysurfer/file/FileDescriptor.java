d007833c390b82398febaa6ec7682349a7b518f2
markpippins <mpippins@gmail.com>
2025-09-30 14:34:34 -0400

    started file-api

---
+++ file-api/src/main/java/com/angrysurfer/file/FileDescriptor.java
@@ -0,0 +1,20 @@
+package com.angrysurfer.file;
+
+
+
+public class FileDescriptor {
+
+
+    private String name;
+    private String path;
+    private String type;
+    private String size;
+    private String lastModified;
+    private String lastModifiedDate;
+    private String url;
+    private String thumbnailUrl;
+    private String deleteUrl;
+    private String deleteType;
+    
+
+}
