6a9983ae3b6d7eaf86a46b6134368f1e2d4da6c0
mpippins <mpippins@gmail.com>
2025-10-16 03:04:09 +0000

    h2

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,17 +1,15 @@
-c# To learn more about how to use Nix to configure your environment
+# To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.jdk
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python3
+    pkgs.nodejs
+    pkgs.maven
   ];
 
   # Sets environment variables in the workspace
