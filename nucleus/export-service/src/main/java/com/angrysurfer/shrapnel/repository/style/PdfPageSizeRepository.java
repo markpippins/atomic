7065e03bc15c2b3cda30803ae501d412a5513e95
mpippins <mpippins@gmail.com>
2025-10-17 00:40:14 +0000

    broken

--- nucleus/export-service/src/main/java/com/angrysurfer/shrapnel/repository/style/PdfPageSizeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.angrysurfer.shrapnel.repository.style;
-
-import com.angrysurfer.shrapnel.model.style.PdfPageSize;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PdfPageSizeRepository extends JpaRepository< PdfPageSize, String> {
-    PdfPageSize findByName(String name);
-}
